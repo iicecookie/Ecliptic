591e2a2bb31cc0efc06c36f7b55e2ede731b852a
binov maxim <maximbu@mail.ru>
2020-04-18 18:37:48 +0300

    Cделана форма для поиска кратчайшего пути

--- Ecliptic/Views/WayFounder/FindWayPage.xaml.cs
+++ Ecliptic/Views/WayFounder/FindWayPage.xaml.cs
@@ -12,12 +12,81 @@ namespace Ecliptic.Views
 {
     public partial class FindWayPage : ContentPage
     {
-
         public FindWayPage()
         {
             InitializeComponent();
+            stackBar1.HeightRequest = 1;
+            stackBar2.HeightRequest = 1;
         }
 
+        #region TextChanged
+        void OnTextFromChanged(object sender, EventArgs e)
+        {
+            SearchBar searchBar = (SearchBar)sender;
+
+            if (searchBar1.Text == "")
+            {
+                searchResults1.ItemsSource = new List<Room>();
+                stackBar1.HeightRequest = 1;
+                return;
+            }
+
+            var searchedrooms = RoomData.Rooms
+                      .Where(room => room.Name   .ToLower().Contains(searchBar1.Text.ToLower()) ||
+                                     room.Details.ToLower().Contains(searchBar1.Text.ToLower()))
+                      .ToList<Room>();
+
+            stackBar1.HeightRequest    = searchedrooms.Count() * 50 + 25;
+            searchResults1.ItemsSource = searchedrooms;
+        }
+        void OnTextToChanged(object sender, EventArgs e)
+        {
+            SearchBar searchBar = (SearchBar)sender;
+
+            if (searchBar2.Text == "")
+            {
+                searchResults2.ItemsSource = new List<Room>();
+                stackBar2.HeightRequest = 1;
+                return;
+            }
+
+            var searchedrooms = RoomData.Rooms
+                      .Where(room => room.Name   .ToLower().Contains(searchBar2.Text.ToLower()) ||
+                                     room.Details.ToLower().Contains(searchBar2.Text.ToLower()))
+                      .ToList<Room>();
+
+            stackBar2.HeightRequest    = searchedrooms.Count() * 50 + 25;
+            searchResults2.ItemsSource = searchedrooms;
+
+        }
+        #endregion
+
+        #region ItemTapped
+        private void OnRoomFromTapped(object sender, ItemTappedEventArgs e)
+        {
+            Room room = (e.Item as Room);
+            searchBar1.Text = room.Name;
+            stackBar1.HeightRequest = 1;
+            searchResults1.ItemsSource = new List<Room>();
+        }
+
+        private void OnRoomToTapped(object sender, ItemTappedEventArgs e)
+        {
+            Room room = (e.Item as Room);
+            searchBar2.Text = room.Name;
+            stackBar2.HeightRequest = 1;
+            searchResults2.ItemsSource = new List<Room>();
+        }
+        #endregion
+        
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            // сначала проверить, есть ли записаные аудитории в базе данных
+            // вывести предупреждение если нет
+
+            // если все окей - производить поиск
+
+        }
     }
 
 }
